9cfe3ce2837aa9aaaae5acb9e33f6dc5559a1f30
Subhraneel2003 <Subhraneel.Das2022@iem.edu.in>
2025-06-27 12:58:20 +0530

    Render Changes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import nltk
 from pydantic import BaseModel
 from app.models_util import predict_sentiment
-app=FastAPI()
+from contextlib import asynccontextmanager
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    nltk.download('stopwords')
+    nltk.download('punkt')  # only if you're using it
+    yield
+
+app = FastAPI(lifespan=lifespan)
 class TextInput(BaseModel):
     text: str
 
